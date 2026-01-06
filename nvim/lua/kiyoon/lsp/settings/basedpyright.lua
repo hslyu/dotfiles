411e4ef0c3b88900345378f46199f3f085f1b7bf
Lyu Hyeonsu <hslyu4@gmail.com>
2026-01-06 13:42:03 +0900

    Add python interpreter

--- nvim/lua/kiyoon/lsp/settings/basedpyright.lua
+++ nvim/lua/kiyoon/lsp/settings/basedpyright.lua
@@ -1,3 +1,5 @@
+local py = (vim.env.CONDA_PREFIX and (vim.env.CONDA_PREFIX .. "/bin/python")) or "python3"
+
 local M = {
   -- handlers = {
   --   ["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
@@ -8,7 +10,10 @@ local M = {
   --   }),
   -- },
   settings = {
-    -- python = {
+    python = {
+      pythonPath = py,
+      defaultInterpreterPath = py,
+    },
     basedpyright = {
       analysis = {
         typeCheckingMode = "standard", -- off, basic, standard, strict, all
